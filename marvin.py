ba11958b5e29afd60e7e92361816f0e0d05e8b9b
roshini <roshini@roshini-H310M-H-2-0>
2026-02-11 15:20:23 +0530

    marvin update

--- marvin.py
+++ marvin.py
@@ -1,4 +1,10 @@
-paranoid_android = "Marvin" # strings are immutable
+paranoid_android = "Marvin, the Paranoid Android" # strings are immutable
 letters = list(paranoid_android) #lists are mutable
-for char in letters:
+for char in letters[:6]:
     print('\t',char)
+print()
+for char in letters[-7:]:
+    print('\t'*2, char)
+print()
+for char in letters[12:20]:
+    print('\t' *3, char)
