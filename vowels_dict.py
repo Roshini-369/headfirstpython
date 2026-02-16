32cc90c30355ceff6cf4427193898fd1a2bd472f
roshini <roshini@cmcvellore.ac.in>
2026-02-16 12:54:20 +0530

    vowels_dict update

--- vowels_dict.py
+++ vowels_dict.py
@@ -2,15 +2,15 @@
 vowels = ['a','e','i','o','u']
 word = input("Enter a word to search for vowels:")
 found = {}
-found['a'] = 0
-found['e'] = 0
-found['i'] = 0
-found['o'] = 0
-found['u'] = 0
+# found['a'] = 0
+# found['e'] = 0
+# found['i'] = 0
+# found['o'] = 0
+# found['u'] = 0
 
 for letter in word:
    if letter in vowels:
+        found.setdefault(letter, 0)
         found[letter] += 1
-for k,v in found.items():
-#for vowel in found:
+for k,v in sorted(found.items()):
     print(k ,"was found",v,"time(s)")
